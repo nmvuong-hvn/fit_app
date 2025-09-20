520d38c4f9cdfa10e541069b5f50a34de46c896b
nmvuong <nmvuong@humaxdigital.com>
2025-09-20 22:13:53 +0700

    Implement home screen

--- app/src/main/java/com/marusys/fitnessapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/marusys/fitnessapp/feature/home/HomeScreen.kt
@@ -1,30 +1,70 @@
 package com.marusys.fitnessapp.feature.home
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.marusys.fitnessapp.R
+import com.marusys.fitnessapp.component.ItemCategory
+import com.marusys.fitnessapp.component.WorkoutItem
+import com.marusys.fitnessapp.model.Category
+import com.marusys.fitnessapp.model.typeExerciseList
+import com.marusys.fitnessapp.ui.theme.FitnessAppTheme
+import com.marusys.fitnessapp.ui.theme.H2ExtraBoldStyle
+import com.marusys.fitnessapp.ui.theme.LocalMyTypography
+import com.marusys.fitnessapp.ui.theme.MyTypograph
+
+
+val backgroundLinearGradient = Brush.linearGradient(
+    listOf(
+        Color(0xFF9747FF),
+        Color(0xFF6F00FF)
+    )
+)
 
 @Composable
 fun FitnessBanner(
@@ -32,26 +72,30 @@ fun FitnessBanner(
     subtitle: String,
     onStartClick: () -> Unit
 ) {
-    Box(
+
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
-            .height(140.dp)
+            .height(176.dp)
             .clip(RoundedCornerShape(16.dp))
-            .background(Color(0xFF8A4FFF)) // tím giống hình
-            .padding(16.dp)
+            .background(backgroundLinearGradient)
     ) {
-        // Nội dung text bên trái
+
+        val (vBox, imgBanner) = createRefs()
+
         Column(
             modifier = Modifier
-                .align(Alignment.CenterStart)
-                .fillMaxWidth(0.6f),
+                .fillMaxWidth(0.6f)
+                .constrainAs(vBox) {
+                    top.linkTo(parent.top, 16.dp)
+                    start.linkTo(parent.start, 16.dp)
+                },
             verticalArrangement = Arrangement.Center
         ) {
             Text(
                 text = title,
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp,
-                color = Color.White
+                color = colorResource(R.color.white),
+                style = LocalMyTypography.current.h2ExtraBoldStyle,
             )
             Spacer(modifier = Modifier.height(8.dp))
             Button(
@@ -61,21 +105,127 @@ fun FitnessBanner(
             ) {
                 Text(
                     text = "Start Exercise",
-                    color = Color(0xFF8A4FFF),
-                    fontWeight = FontWeight.SemiBold
+                    color = colorResource(R.color.primary),
+                    style = LocalMyTypography.current.body2Bold
                 )
             }
         }
-
-        // Hình nhân vật bên phải
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background), // thay bằng ảnh thật của bạn
+            painter = painterResource(id = R.drawable.img_banner),
             contentDescription = "Fitness Person",
             modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .height(120.dp)
+                .constrainAs(imgBanner) {
+                    end.linkTo(parent.end, 14.dp)
+                    bottom.linkTo(parent.bottom)
+                }
+                .width(126.dp)
+                .height(135.dp)
                 .padding(end = 4.dp),
-            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val TAG = "HomeScreen"
+    LazyColumn (modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+        ){
+        item {
+            Spacer(Modifier.height(9.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Flexio", style = LocalMyTypography.current.h3BoldStyle)
+                Image(imageVector = ImageVector.vectorResource(R.drawable.notification), "notificaiton")
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            FitnessBanner("Start Strong and Set Your Fitness Goals", "") { }
+            Spacer(modifier = Modifier.height(40.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Progress", style = LocalMyTypography.current.h3BoldStyle)
+                Text("See All", style = LocalMyTypography.current.body2Semi, color = colorResource(R.color.primary))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyRow(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                items(10){
+                    WorkoutItem(
+                        title = "Jumping Jacks",
+                        remainingTime = "2:30",
+                        progress = 0.5f,
+                        current = 5,
+                        total = 10
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Categories",style = LocalMyTypography.current.h3BoldStyle)
+                Text("See All", style = LocalMyTypography.current.body2Semi, color = colorResource(R.color.primary))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+//            LazyRow(modifier = Modifier.fillMaxWidth(),
+//                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                items(10){
+//                    ItemCategoryExercise( it == 1)
+//                }
+//            }
+//            Spacer(modifier = Modifier.height(24.dp))
+        }
+        items(typeExerciseList.size){
+            ItemCategory(data = typeExerciseList[it]){}
+        }
+        item { Spacer(modifier = Modifier.height(48.dp)) }
+    }
+
+}
+
+@Composable
+fun ItemCategoryExercise(
+    isSelected : Boolean = false
+){
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier
+            .wrapContentWidth()
+            .height(37.dp)
+            .drawBehind {
+                drawRoundRect(
+                    color = if (isSelected)
+                        Color(context.getColor(R.color.primary)) else Color(
+                        context.getColor(R.color.gray_3)
+                    ),
+                    style = Stroke(width = 1f),
+                    size = size,
+                    cornerRadius = CornerRadius(32f, 32f)
+                )
+            }
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+
+    ) {
+        Text(
+            "All", color = if (isSelected)
+                Color(context.getColor(R.color.primary)) else Color(
+                context.getColor(R.color.gray_3)
+            )
         )
     }
 }
@@ -83,9 +233,9 @@ fun FitnessBanner(
 @Preview
 @Composable
 fun FitnessBannerPreview() {
-    FitnessBanner(
-        title = "Ready for your next workout?",
-        subtitle = "Let's get moving!",
-        onStartClick = {}
-    )
+    FitnessAppTheme {
+        CompositionLocalProvider(LocalMyTypography provides MyTypograph()) {
+            HomeScreen(modifier = Modifier.fillMaxSize())
+        }
+    }
 }
--- app/src/main/java/com/marusys/fitnessapp/model/Category.kt
+++ app/src/main/java/com/marusys/fitnessapp/model/Category.kt
@@ -3,6 +3,7 @@ package com.marusys.fitnessapp.model
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Stable
 import com.marusys.fitnessapp.R
+import kotlinx.collections.immutable.persistentListOf
 
 @Stable
 data class Category(
@@ -35,3 +36,15 @@ data class ExerciseProgress(
     val exerciseId: Int = 0,
     val timeTotal : Long = 0
 )
+
+
+val typeExerciseList = persistentListOf(
+    Category(1, "Full Body Warm Up", R.drawable.exercise_01, ""),
+    Category(2, "Strength Exercise", R.drawable.exercise_02, ""),
+    Category(3, "Both Side Plank", R.drawable.exercise_03, ""),
+    Category(4, "Abs Workout", R.drawable.exercise_04, ""),
+    Category(5, "Torso and Trap Workout", R.drawable.exercise_05, ""),
+    Category(6, "Lower Back Exercise", R.drawable.exercise_06, "")
+)
+
+
